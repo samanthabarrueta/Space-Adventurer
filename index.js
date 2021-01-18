3949bd6341775bce08f5bf7ec60dec66b87c5285
Samantha <samanthabarrueta@gmail.com>
2021-01-17 22:20:00 -0600

    move left and right with arrow keys

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
+$(document).keydown(function(e) {
+    switch (e.which) {
+    case 37:
+        $('#user').finish().animate({
+            left: "-=50"
+        });
+        break;
+    case 39:
+        $('#user').finish().animate({
+            left: "+=50"
+        });
+        break;
+    };
+});
 
