7a93f9a19816c7a32f75842a3cba6c55b1c8ec0d
Samantha <samanthabarrueta@gmail.com>
2021-01-17 23:57:24 -0600

    shoot bullet on space key

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
         $('#user').finish().animate({
-            left: "-=50"
+            left: '-=50'
         });
         break;
     case 39:
         $('#user').finish().animate({
-            left: "+=50"
+            left: '+=50'
+        });
+        break;
+    case 32:
+        $('#bullet').animate({
+            bottom: '+=620'
         });
         break;
     };
