14c64a391f036e2d541f9e6f25783a10ebf5e016
Samantha <samanthabarrueta@gmail.com>
2021-01-25 22:43:11 -0600

    create villain/ villain class

--- index.js
+++ index.js
@@ -3,3 +3,31 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+class Villain {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = 15;
+        this.weight = 2;
+        this.direction = 1;
+    };
+    update() {
+        this.y += this.weight;
+    };
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    };
+};
+const villain1 = new Villain(50, 50);
+
+const animate = () => {
+    villain1.update();
+    villain1.draw();
+};
+
+animate();
+
