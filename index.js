bc328abe8eeb619e8d05375ba537efa98635dc47
Samantha <samanthabarrueta@gmail.com>
2021-02-02 22:13:23 -0600

    animate user

--- index.js
+++ index.js
@@ -4,40 +4,45 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
        
 let villainsArray = [];
+let playerArray = [];
 
 class Player {
     constructor(x) {
         this.x = canvas.width/2;
         this.y = canvas.height;
+        this.dx = 5;
         this.size = 25;
         this.weight = 2;
-        this.direction = 1;
-        this.speed = 20;
+        this.velocity = {
+            x: 1,
+            y: 1
+        };
     };
     draw() {
+        // document.addEventListener('keydown', doKeyDown, true);
         ctx.fillStyle = 'blue';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
-    };
+    }; 
     update() {
         this.draw();
-        this.move(); 
-    };
-    move() {
-        document.addEventListener('keydown', doKeyDown, true);
+        this.x = this.x + this.velocity.x;
+        console.log(this.x)
     };
 };
 
-const doKeyDown = (key) => {
-    if(key.keyCode == 37) { 
-        console.log('left')
-    };
-    if(key.keyCode == 39) {
-        console.log('right')
-    };
-};
+// const doKeyDown = (key) => {
+//     if(key.keyCode == 37) { 
+//         this.x -= this.dx;
+//         console.log(this.x)
+//     };
+//     if(key.keyCode == 39) {
+//         this.x += this.dx;
+//         console.log(this.x)
+//     };
+// };
 
 class Villain {
     constructor(x) {
@@ -70,14 +75,32 @@ const spawnVillains = () => {
     }, 1500);
 };
 
+const spawnPlayers = () => {
+    setInterval(() => {
+        playerArray.push(new Player())
+    }, 1500);
+};
+
 const animate = () => {
     requestAnimationFrame(animate);
     villainsArray.forEach((villain) => {
         villain.update()
     });
-    const player1 = new Player();
-    player1.update();
+    playerArray.forEach((player) => {
+        player.update()
+    });
 };
 
 animate();
 spawnVillains();
+spawnPlayers();
+
+// var lastDownTarget, canvas; 
+// window.onload = function() { 
+// canvas = document.getElementById('canvas'); 
+
+// /* For mouse event */ 
+// document.addEventListener('mousedown', function(event) { 
+// lastDownTarget = event.target; 
+// console.log('Mousedown Event'); 
+// }, false); 
