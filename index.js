38d21ec209f55805ee13635f414a536fdbbecde0
Samantha <samanthabarrueta@gmail.com>
2021-02-09 21:04:41 -0600

    refactor
    remove logs, alerts, update object properties

--- index.js
+++ index.js
@@ -6,27 +6,25 @@ canvas.height = window.innerHeight;
 let villainsArray = [];
 
 class Player {
-    constructor(x, y) {
+    constructor() {
         this.x = canvas.width/2;
-        this.y = y;
+        this.y = canvas.height;
         this.dx = 10;
-        this.size = 25;
+        this.radius = 25;
         this.weight = 2;
     };
     draw() {
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
     }; 
     moveRight() {
         this.x += this.dx;
-        console.log('moveRight' + this.x);
     };
     moveLeft() {
         this.x -= this.dx;
-        console.log('moveLeft' + this.x);
     };
     update() {
         this.draw();
@@ -35,18 +33,16 @@ class Player {
 
 const doKeyDown = (key) => {
     if(key.keyCode == 37) { 
-        alert(key.keyCode);
         player.moveLeft();
         player.update();
     };
     if(key.keyCode == 39) {
-        alert(key.keyCode);
         player.moveRight();
         player.update();
     };
 };
 
-let player = (new Player(canvas.width/2, canvas.height));
+let player = (new Player());
 player.update();
 addEventListener('keydown', doKeyDown, true);
 
